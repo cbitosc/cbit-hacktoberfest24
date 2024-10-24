c11a62a4d7d45e551ef2aae3994a8e2795231973
mdimado <mdimad005@gmail.com>
2024-10-24 14:37:09 +0530

    feat: update chatbot with Hacktoberfest context

--- src/components/chatbot.js
+++ src/components/chatbot.js
@@ -21,6 +21,8 @@ const Chatbot = ({ chatHistory, setChatHistory }) => {
     name: "CBIT Hacktoberfest Hackathon'24"
     type: "24-hour virtual hackathon"
     dates: "October 26-27, 2024"
+    time: "26th October 4pm - 27th October 8:30pm"
+    cash prize: "Not to be disclosed yet"
     registration:
       opens: "October 8, 2024, 6 PM"
       fee: "Free"
@@ -28,7 +30,7 @@ const Chatbot = ({ chatHistory, setChatHistory }) => {
     mode: "Online- through Discord"
     eligibility: "High school to final year bachelor's degree students in any field"
   
-  cosc_team:
+  cosc_team, core_committee, mentors:
     president: "Matta Sai Kiran Goud"
     vice_president: "Akil Krishna"
     head_of_external_affairs: "Kousik Reddy"
@@ -41,6 +43,24 @@ const Chatbot = ({ chatHistory, setChatHistory }) => {
       - "G Harshith"
       - "Nithin Konda"
       - "Garlapati Ritesh"
+ cosc_organising_committee: 
+    - "Meghana Sancheti"
+    - "Mohammed Imaddudin" 
+    - "Muzzafar"
+    - "Amulya Motta"
+    - "Jayanth Gudimella"
+    - "Sharanya Peri"
+    - "Sneha Arumugam" 
+    - "Srilekha Kasha"
+    - "Sriya Palaparti"
+    - "Kavyashree Poojari"
+    - "Rishi Mamilla"
+    - "Doma Akshitha Reddy"
+    - "Ajith Sai Chekka"
+    - "Harshavardhan Reddy"
+    - "Anirudh Reddy"
+    
+   
   
   
   participation_info:
@@ -49,14 +69,27 @@ const Chatbot = ({ chatHistory, setChatHistory }) => {
   
   response_guidelines:
     - "Answer questions briefly"
+    - "this is beginner friendly"
     - "Offer insights about Hacktoberfest, open source, and Preptember"
     - "Core Committee Members are president, vice_president, head_of_external_affairs, general_secretaries, joint_secretaries."
     - "When asked about COSC members, mention the core committee members and just mention that there are other organising Committee Members"
     - "Guide participants to the Preptember page for more informative videos"
     - "Cross institution teams are allowed."
-    - "Do not provide details about COSC members not listed; direct users to the contact page"
+    - "Direct users to the contact page when asked questions you cant answer"
     - "Do not derogate any person or entity under any circumstance"
     - "If unable to answer, direct participants to contact us section on the website"
+    - "Handbook will be released soon"
+    - "Further details will be posted in the whatsapp group you join after registration."
+    - "Everyone who participates will get a participation certificate."
+    - "Problem statements will be released on 6pm on 26th October"
+    - "only the team leader will be joining the whatsapp group"
+    - "You can join discord server on 26th october"
+    - "One person cannot be a part of more than one team" 
+    - "If you have registered on Unstop or Hack2Skill, please register through the website as well"
+    - "Teams of 1 and 2 will be automatically paired up by the OC to meet the 3-5 participants criteria."
+    - "the team lead can add participants to it's team until the deadline for registrations." 
+    - "For the hackathon, you will need a laptop with a microphone, camera and a good connection"
+    - "Refer to the preptember page for tech stack ideas" 
   `;
 
   const typeMessage = async (message) => {
